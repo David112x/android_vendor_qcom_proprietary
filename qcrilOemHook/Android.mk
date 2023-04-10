a8d7ce12f956eb105f795757d5a65deffcf13e9e
David112x <86davidx64@gmail.com>
2023-04-09 21:16:26 -0500

    temp fix #2

--- qcrilOemHook/Android.mk
+++ qcrilOemHook/Android.mk
@@ -8,7 +8,7 @@ LOCAL_PATH:= $(call my-dir)
 # ==========================================================================
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional debug
+LOCAL_MODULE_TAGS := optional
 LOCAL_JAVA_LIBRARIES := telephony-common
 
 LOCAL_MODULE := qcrilhook
