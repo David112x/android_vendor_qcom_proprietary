1ed7790744022ae989c38074836f2cd701efa5a5
David112x <86davidx64@gmail.com>
2023-04-09 22:58:14 -0500

    telephony-apps: Fix DataStatusNotification Android.mk

--- telephony-apps/DataStatusNotification/Android.mk
+++ telephony-apps/DataStatusNotification/Android.mk
@@ -2,12 +2,12 @@ ifneq ($(TARGET_NO_TELEPHONY), true)
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional eng
+LOCAL_MODULE_TAGS := optional
 
 LOCAL_PROGUARD_ENABLED := disabled
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
-
+LOCAL_SDK_VERSION := current
 LOCAL_PACKAGE_NAME := datastatusnotification
 LOCAL_CERTIFICATE := platform
 LOCAL_JAVA_LIBRARIES := qcrilhook telephony-common
